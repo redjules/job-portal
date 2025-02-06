31413706b754d71c9619ab6b3dc3460103982f3e
Ana <anapedrabcn@gmail.com>
2025-02-06 13:20:27 +0100

    fixed buttons

--- src/hooks/use-fetch.jsx
+++ src/hooks/use-fetch.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { useSession } from "@clerk/clerk-react";
 
-const useFetch = (cb, (options = {})) => { 
+const useFetch = (cb, options = {}) => {
   const [data, setData] = useState(undefined);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
@@ -12,22 +13,20 @@ const useFetch = (cb, (options = {})) => {
     setError(null);
 
     try {
-        const supabaseAccessToken = await session.getToken({
-          template: "supabase",
-        });
-        const response = await cb(supabaseAccessToken, options, ...args);
-        setData(response);
-        setError(null);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+      const supabaseAccessToken = await session.getToken({
+        template: "supabase",
+      });
+      const response = await cb(supabaseAccessToken, options, ...args);
+      setData(response);
+      setError(null);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return { data, loading, error, fn };
-
-
+  return { data, loading, error, fn };
 };
 
 export default useFetch;
--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -2,65 +2,66 @@ import { useUser } from "@clerk/clerk-react";
 import { useParams } from "react-router-dom";
 import useFetch from "@/hooks/use-fetch";
 
-
 const JobPage = () => {
+  const { isLoaded, user } = useUser();
+  const { id } = useParams();
 
-    const { isLoaded, user } = useUser();
-    const { id } = useParams();
+  const {
+    loading: loadingJob,
+    data: job,
+    fn: fnJob,
+  } = useFetch(getSingleJob, {
+    job_id: id,
+  });
 
-    const {
-        loading: loadingJob,
-        data: job,
-        fn: fnJob,
-      } = useFetch(getSingleJob, {
-        job_id: id,
-      });
+  const { loading: loadingHiringStatus, fn: fnHiringStatus } = useFetch(
+    updateHiringStatus,
+    {
+      job_id: id,
+    }
+  );
 
-      const { loading: loadingHiringStatus, fn: fnHiringStatus } = useFetch(
-        updateHiringStatus,
-        {
-          job_id: id,
-        }
-      );
+  const handleStatusChange = (value) => {
+    const isOpen = value === "open";
+    fnHiringStatus(isOpen).then(() => fnJob());
+  };
 
-      const handleStatusChange = (value) => {
-        const isOpen = value === "open";
-        fnHiringStatus(isOpen).then(() => fnJob());
-      };
-    
+  useEffect(() => {
+    if (isLoaded) fnJob();
+  }, [isLoaded]);
 
-
-
-      useEffect(() => {
-        if (isLoaded) fnJob();
-      }, [isLoaded]);
-
-      if (!isLoaded || loadingJob) {
-        return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
-      }
-
+  if (!isLoaded || loadingJob) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
 
   return (
-  <div className="flex flex-col gap-8 mt-5">
-    <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
-        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl>{job?.title}</h1>
+    <div className="flex flex-col gap-8 mt-5">
+      <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
+        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
+          {job?.title}
+        </h1>
         <img src={job?.company?.logo_url} className="h-12" alt={job?.title} />
-    </div>
+      </div>
 
-    <div className="flex justify-between ">
-        <div>
-            {job?.location}
+      <div className="flex justify-between ">
+        <div>{job?.location}</div>
+        <div className="flex gap-2">
+          <Briefcase /> {job?.applications?.length} Applicants
         </div>
         <div className="flex gap-2">
-            <Briefcase/> {job?.applications?.length} Applicants
+          {job?.isOpen ? (
+            <>
+              <DoorOpen /> Open
+            </>
+          ) : (
+            <>
+              <DoorClosed /> Closed
+            </>
+          )}
         </div>
-        <div className="flex gap-2">
-            {job?.isOpen?<><DoorOpen/> Open</>):(<><DoorClosed /> Closed</>
-        )}
-        </div>
-    </div>
+      </div>
 
-    {job?.recruiter_id === user?.id && (
+      {job?.recruiter_id === user?.id && (
         <Select onValueChange={handleStatusChange}>
           <SelectTrigger
             className={`w-full ${job?.isOpen ? "bg-green-950" : "bg-red-950"}`}
@@ -78,10 +79,10 @@ const JobPage = () => {
         </Select>
       )}
 
-    <h2 className="text-2xl sm:text-3xl font-bold">About the job</h2>
-    <p className="sm:text-lg">{job?.description}</p>
-    
-    <h2 className="text-2xl sm:text-3xl font-bold">
+      <h2 className="text-2xl sm:text-3xl font-bold">About the job</h2>
+      <p className="sm:text-lg">{job?.description}</p>
+
+      <h2 className="text-2xl sm:text-3xl font-bold">
         What we are looking for
       </h2>
       <MDEditor.Markdown
@@ -89,16 +90,16 @@ const JobPage = () => {
         className="bg-transparent sm:text-lg" // add global ul styles - tutorial
       />
 
-        {job?.recruiter_id !== user?.id && (
-                <ApplyJobDrawer
-                job={job}
-                user={user}
-                fetchJob={fnJob}
-                applied={job?.applications?.find((ap) => ap.candidate_id === user.id)}
-                />
-            )}
+      {job?.recruiter_id !== user?.id && (
+        <ApplyJobDrawer
+          job={job}
+          user={user}
+          fetchJob={fnJob}
+          applied={job?.applications?.find((ap) => ap.candidate_id === user.id)}
+        />
+      )}
 
-    {loadingHiringStatus && <BarLoader width={"100%"} color="#36d7b7" />}
+      {loadingHiringStatus && <BarLoader width={"100%"} color="#36d7b7" />}
       {job?.applications?.length > 0 && job?.recruiter_id === user?.id && (
         <div className="flex flex-col gap-2">
           <h2 className="font-bold mb-4 text-xl ml-1">Applications</h2>
@@ -110,10 +111,6 @@ const JobPage = () => {
         </div>
       )}
     </div>
-
-
-    </div>
-
   );
 };
 
